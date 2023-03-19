5f6344428901beaf81cf5f925658c944c6b512d0
naeunk1m <08enmik@gmail.com>
2023-03-20 06:21:40 +0900

    20230320

--- src/App.js
+++ src/App.js
@@ -1,128 +1,114 @@
-import React, {
-  useCallback,
-  useEffect,
-  useMemo,
-  useReducer,
-  useRef,
-  createContext
-} from "react";
-import RecordEditor from "./RecordEditor";
-import List from "./List";
-import "./App.css";
+import React, { useEffect, useReducer, useRef } from "react";
 
-export const DiaryStateContext = createContext(null);
-export const DiaryDispatchContext = createContext(null);
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import Home from "./pages/Home";
+import New from "./pages/New";
+import Edit from "./pages/Edit";
+import Diary from "./pages/Diary";
 
 const reducer = (state, action) => {
+  let newState = [];
   switch (action.type) {
     case "INIT": {
       return action.data;
     }
     case "CREATE": {
-      const created_date = new Date().getTime();
-
-      const newItem = {
-        ...action.data,
-        created_date
-      };
-      return [newItem, ...state];
+      newState = [action.data, ...state];
+      break;
     }
     case "REMOVE": {
-      return state.filter((it) => it.id !== action.targetId);
+      newState = state.filter((it) => it.id !== action.targetId);
+      break;
     }
     case "EDIT": {
-      return state.map((it) =>
-        it.id === action.targetId
-          ? {
-            ...it,
-            content: action.newContent
-          }
-          : it
+      newState = state.map((it) =>
+        it.id === action.data.id ? { ...action.data } : it
       );
+      break;
     }
     default:
       return state;
   }
+
+  localStorage.setItem("diary", JSON.stringify(newState));
+  return newState;
 };
 
-const App = () => {
+export const DiaryStateContext = React.createContext();
+export const DiaryDispatchContext = React.createContext();
+
+function App() {
   const [data, dispatch] = useReducer(reducer, []);
-  const dataId = useRef(0);
-  const getData = async () => {
-    setTimeout(async () => {
-      const res = await fetch(
-        "https://jsonplaceholder.typicode.com/comments"
-      ).then((res) => res.json());
-
-      const initData = res.slice(0, 20).map((it) => {
-        return {
-          author: it.email,
-          content: it.body,
-          emotion: Math.floor(Math.random() * 5) + 1,
-          created_date: new Date().getTime(),
-          id: dataId.current++
-        };
-      });
-
-      dispatch({ type: "INIT", data: initData });
-    }, 2000);
-  };
 
   useEffect(() => {
-    getData();
+    const localData = localStorage.getItem("diary");
+    if (localData) {
+      const diaryList = JSON.parse(localData).sort(
+        (a, b) => parseInt(b.id) - parseInt(a.id)
+      );
+
+      if (diaryList.length >= 1) {
+        dataId.current = parseInt(diaryList[0].id) + 1;
+        dispatch({ type: "INIT", data: diaryList });
+      }
+    }
   }, []);
 
-  const onCreate = useCallback((author, content, emotion) => {
+  const dataId = useRef(0);
+  // CREATE
+  const onCreate = (date, content, emotion) => {
     dispatch({
       type: "CREATE",
-      data: { author, content, emotion, id: dataId.current }
+      data: {
+        id: dataId.current,
+        date: new Date(date).getTime(),
+        content,
+        emotion,
+      },
     });
     dataId.current += 1;
-  }, []);
-
-  const onRemove = useCallback((targetId) => {
+  };
+  // REMOVE
+  const onRemove = (targetId) => {
     dispatch({ type: "REMOVE", targetId });
-  }, []);
-
-  const onEdit = useCallback((targetId, newContent) => {
+  };
+  // EDIT
+  const onEdit = (targetId, date, content, emotion) => {
     dispatch({
       type: "EDIT",
-      targetId,
-      newContent
+      data: {
+        id: targetId,
+        date: new Date(date).getTime(),
+        content,
+        emotion,
+      },
     });
-  }, []);
-
-  const memoizedDiaryAnalysis = useMemo(() => {
-    const goodCount = data.filter((it) => it.emotion >= 3).length;
-    const badCount = data.length - goodCount;
-    const goodRatio = (goodCount / data.length) * 100.0;
-    return { goodCount, badCount, goodRatio };
-  }, [data.length]);
-
-  const { goodCount, badCount, goodRatio } = memoizedDiaryAnalysis;
-
-  const store = {
-    data
   };
 
-  const memoizedDispatch = useMemo(() => {
-    return { onCreate, onRemove, onEdit };
-  }, []);
-
   return (
-    <DiaryStateContext.Provider value={store}>
-      <DiaryDispatchContext.Provider value={memoizedDispatch}>
-        <div className="App">
-          <RecordEditor />
-          <div>전체 일기 : {data.length}</div>
-          <div>기분 좋은 일기 개수 : {goodCount}</div>
-          <div>기분 나쁜 일기 개수 : {badCount}</div>
-          <div>기분 좋은 일기 비율 : {goodRatio}%</div>
-          <List />
-        </div>
+    <DiaryStateContext.Provider value={data}>
+      <DiaryDispatchContext.Provider
+        value={{
+          onCreate,
+          onEdit,
+          onRemove,
+        }}
+      >
+        <BrowserRouter>
+          <div className="App">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/new" element={<New />} />
+              <Route path="/edit/:id" element={<Edit />} />
+              <Route path="/diary/:id" element={<Diary />} />
+            </Routes>
+          </div>
+        </BrowserRouter>
       </DiaryDispatchContext.Provider>
     </DiaryStateContext.Provider>
   );
-};
+}
 
 export default App;
